ae4d0bdf2ba18b4943d9ef0c3fe63889081e6cb7
Subham-102 <subhamkrshawa21s@gmail.com>
2026-06-17 22:01:45 +0530

    Updated frontend API to point to live Render backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // backend base URL
+  baseURL: "https://dbnorm-api.onrender.com", // backend base URL
   headers: { "Content-Type": "application/json" }
 });
 
